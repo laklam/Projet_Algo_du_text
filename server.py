7fbeb6854b2afec37fcdb72f6f3ef3aacccf8e50
FaizaBerhili <52208907+FaizaBerhili@users.noreply.github.com>
2020-05-02 12:49:15 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,20 +1,29 @@
 from flask import Flask, render_template, request, jsonify, url_for, redirect
 
-#liste de test pour affichage des résultats
+#liste de test pour l'affichage des résultats
 #list_url=[['univ','https://www.univ-paris13.fr/'],['ent','https://cas.univ-paris13.fr/cas/login?service=https%3A%2F%2Fent.univ-paris13.fr']]
 
+#initialisation de la liste qui récupère les résultats
 list_url=[]
+
+#variable globale permettant de garder la dernière valeur entrée par l'utilisateur
+search =""
+
 #création objet app
 app = Flask(__name__, template_folder='.')
 @app.route('/') #racine
 def index():
-  search = request.args.get('search')
   return render_template('index.html')
 
-@app.route('/pageResults' , methods = ['GET', 'POST'])
+@app.route('/pageResults' , methods = ['POST'])
 def pageResults():
+    global search
     search = request.form['search']
-    return render_template('pageResults.html', search=search, list_url=list_url)    
+    return render_template('pageResults.html', search=search, list_url=list_url)
+
+@app.route('/pageResults')
+def pageResult():
+  return render_template('pageResults.html', search=search, list_url=list_url)
 
 if __name__ == '__main__':
     app.run(debug=True)
