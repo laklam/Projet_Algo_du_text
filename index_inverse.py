17148567d45c0682b065e5d4c5a114d41b3f78d5
Ouss16100 <43671851+Ouss16100@users.noreply.github.com>
2020-05-06 02:15:09 +0200

    Add files via upload

---
+++ index_inverse.py
@@ -0,0 +1,9 @@
+class IndexInverse:
+    """
+     class Index Inverser
+    """
+    def __init__(self, nb_url, url_id, mot, frequence):
+        self.nb_url = nb_url
+	self.url_id = url_id
+	self.mot = mot
+	self.frequence = frequence
